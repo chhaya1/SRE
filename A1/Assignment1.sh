2e8ff89cd4d2050f4400972a2ab1303b75bd6e8d
chhaya1 <chhayasharma1401@gmail.com>
2024-10-01 15:45:17 +0530

    correct the format

--- A1/Assignment1.sh
+++ A1/Assignment1.sh
@@ -1,21 +1,21 @@
 # Problem 1: curl command to google.com. Store the output to two files at a time and suppress the output to stdout. (ss)
 
-curl -s https://www.google.com | tee file1.html file2.html > /dev/null
+    curl -s https://www.google.com | tee file1.html file2.html > /dev/null
 
 
 # Problem 4:
 
-# Download the file using command and not the GUI
- wget https://raw.githubusercontent.com/shashikarsiddharth/sre-devops-cohort-examples/main/resources/nginx-dummy.log 
+    # Download the file using command and not the GUI
+    wget https://raw.githubusercontent.com/shashikarsiddharth/sre-devops-cohort-examples/main/resources/nginx-dummy.log 
 
-# Parse the different status code from the Nginx access log file. (ss)
-awk '{print $9}' nginx-dummy.log | sort | uniq -c | sort -nr
+    # Parse the different status code from the Nginx access log file. (ss)
+    awk '{print $9}' nginx-dummy.log | sort | uniq -c | sort -nr
 
 
 # Problem 5: (ss)
 
-# Download the image JSON file.
-wget https://raw.githubusercontent.com/shashikarsiddharth/sre-devops-cohort-examples/main/resources/image.json
+    # Download the image JSON file.
+    wget https://raw.githubusercontent.com/shashikarsiddharth/sre-devops-cohort-examples/main/resources/image.json
 
-# Download the images in the JSON file.
-jq -r '.[].url' image.json | xargs -n 1 curl -O
+    # Download the images in the JSON file.
+    jq -r '.[].url' image.json | xargs -n 1 curl -O
--- A1/Assignment2.sh
+++ A1/Assignment2.sh
@@ -1,34 +1,34 @@
 # Problem 1: ls command
 
-# Includes all files, including hidden files
-ls -a  
-# Sizes are listed in human readable format
-ls -lh
-# Files are ordered by recency
-ls -lht
-# Output is colourised
-ls -lh --color
+  # Includes all files, including hidden files
+  ls -a  
+  # Sizes are listed in human readable format
+  ls -lh
+  # Files are ordered by recency
+  ls -lht
+  # Output is colourised
+  ls -lh --color
 
 # Problem 3: Script that counts the number of files of each type (e.g., .txt, .sh) in a target directory.
-find . -type f | sed 's|^.*/||' | sort | uniq -c | sort -nr
+  find . -type f | sed 's|^.*/||' | sort | uniq -c | sort -nr
 
 # Problem 4: Script that copies all files matching certain pattern from one directory to another. Script should write logs in a file, which can be used for later debugging.
 
 # Problem 5: Bash command hat lists all files in a directory and its sub-directories.
-find ./ -type f
+  find ./ -type f
 
 # Problem 6: Create a script that displays CPU stats, Free RAM, and disk usage and logs it to a file.
-{
-  echo "CPU Stats:"
-  mpstat 1 1 | tail -n +4 | head -n -1
-  echo
-  echo "Free RAM:"
-  free -h
-  echo
-  echo "Disk Usage:"
-  df -h
-} >> system_stats.log
+  {
+    echo "CPU Stats:"
+    mpstat 1 1 | tail -n +4 | head -n -1
+    echo
+    echo "Free RAM:"
+    free -h
+    echo
+    echo "Disk Usage:"
+    df -h
+  } >> system_stats.log
 
 # Problem 7: identify top 5 most expensive processes running on the system in terms of Memory and CPU.
 
-ps aux --sort=%cpu | head -n 6
+  ps aux --sort=%cpu | head -n 6
